5681cdeadbd43e88ca23ce69635eaa469788d930
Fabrice-sangwa <19sm476@esisalama.org>
2022-10-01 11:29:50 +0200

    Exercice sur les slices

---
+++ les_slices/exercie1.py
@@ -0,0 +1,14 @@
+liste = ["Utilisateur_01",
+         "Utilisateur_02",
+         "Utilisateur_03",
+         "Utilisateur_04",
+         "Utilisateur_05",
+         "Utilisateur_06",
+         "Utilisateur_07"
+         ]
+print(liste[:])
+print(liste[::-1])
+print(liste[:3:3])
+print(liste[3::2])
+print(liste[1:-1])
+print(liste[::6])
