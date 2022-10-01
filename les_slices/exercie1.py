16328af2ab64272cf3ad071baac50b8280650fba
Fabrice-sangwa <19sm476@esisalama.org>
2022-10-01 11:39:26 +0200

    Ajout d'une deuxième liste

--- les_slices/exercie1.py
+++ les_slices/exercie1.py
@@ -11,4 +11,15 @@ print(liste[::-1])
 print(liste[:3:3])
 print(liste[3::2])
 print(liste[1:-1])
-print(liste[::6])
+print(liste)
+
+liste = ["Maxime", "Martine", "Christopher", "Carlos", "Michael", "Eric"]
+trois_premiers = liste[:3]
+trois_derniers = liste[3:]
+milieu = liste[1:-1]
+premier_dernier = liste[::5]
+
+print(trois_premiers)
+print(trois_derniers)
+print(milieu)
+print(premier_dernier)
