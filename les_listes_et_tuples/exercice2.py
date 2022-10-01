be93952766d6e72555b524d42e72608dd698832e
Fabrice-sangwa <19sm476@esisalama.org>
2022-10-01 11:07:31 +0200

    Les méthodes d'ajout et de suppresion dans une liste

--- les_listes_et_tuples/exercice2.py
+++ les_listes_et_tuples/exercice2.py
@@ -1,2 +1,12 @@
 
 liste = ["Fabrice", "SANGWA", 1_000_000_000]
+print(liste)
+
+liste.append("il")
+print(liste)
+
+liste.extend(["Un", "deux", 3, 4, 5])
+print(liste)
+
+liste.remove("deux")
+print(liste)
