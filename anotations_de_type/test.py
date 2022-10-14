c66fdf0ce87bc688a888c31804731aca1c2f83ea
Fabrice-sangwa <19sm476@esisalama.org>
2022-10-14 07:58:37 +0200

    Test des anotations de type dans le terminal avec mypy

---
+++ anotations_de_type/test.py
@@ -0,0 +1,8 @@
+a: str = "Fab"
+#a: str = 4
+b: str = "Hello"
+print(f"{b} {a}")
+
+#we want to verify some errors in terminal with mypy
+#you must install firstlly  mypy library : py -m pip install mypy
+
