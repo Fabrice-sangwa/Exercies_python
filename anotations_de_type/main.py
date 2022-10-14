f972d5c5c6837055a3273c7520aa9a1abb92c73b
Fabrice-sangwa <19sm476@esisalama.org>
2022-10-14 07:39:57 +0200

    Les anotations des types sur les fonctions et sur les variables

---
+++ anotations_de_type/main.py
@@ -0,0 +1,12 @@
+def add ( a: int = 0, b: int = 0 ) -> int : 
+    return a + b
+
+def names() -> list[str]:   
+    return ["James", "Karl", "Sonia"]
+
+a: int = 4
+b: float = 4.5
+c: bool = True
+d: str = "Salut"
+e: list = []
+
