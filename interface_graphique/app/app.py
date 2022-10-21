8c8e2b5cfa81f0fceaeca531df74b9aa96c1d68c
Fabrice-sangwa <19sm476@esisalama.org>
2022-10-21 08:40:39 +0200

    Ajout d'un layout et des widgets sur la fenetre principale

--- interface_graphique/app/app.py
+++ interface_graphique/app/app.py
@@ -6,6 +6,33 @@ class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Convertisseur de devise")
+        self.setup_ui()
+        
+        
+    #Créateur de composants de l'interface
+    def setup_ui(self):
+        #on crée un layout horizontal du parent(self)
+        self.layout = QtWidgets.QHBoxLayout(self)
+        #on crée un widget combobox, on a pas besoin de placer le self, on l'ajouter au layout à la fin 
+        self.cbb_devisesFrom = QtWidgets.QComboBox()
+        #on crée un autre widget spinbox 
+        self.spn_montant = QtWidgets.QSpinBox()
+        #encore un autre comboBox et un spinBox
+        self.cbb_devisesTo = QtWidgets.QComboBox()
+        self.spn_montantConverti = QtWidgets.QSpinBox()
+        #on crée un bouton avec le texte à afficher sur le bouton
+        self.btn_inverser = QtWidgets.QPushButton("Inverser les devises")
+        
+        #on va ensuite insérer les widgets dans le layout 
+        
+        self.layout.addWidget(self.cbb_devisesFrom)
+        self.layout.addWidget(self.spn_montant)
+        self.layout.addWidget(self.cbb_devisesTo)
+        self.layout.addWidget(self.spn_montantConverti)
+        self.layout.addWidget(self.btn_inverser)
+        
+        
+    
         
 #création de l'application      
 app  = QtWidgets.QApplication([])
